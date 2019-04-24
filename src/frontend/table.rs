dc43c56e3d7ef77c0b83289c6a8990be384ba8c9
Matthias Beyer <matthias.beyer@atos.net>
2019-04-24 09:45:58 +0200

    Implement TableFrontend::compare_packages()

--- src/frontend/table.rs
+++ src/frontend/table.rs
@@ -3,11 +3,14 @@ use std::ops::Deref;
 
 use librepology::v1::types::Package;
 use librepology::v1::types::Problem;
+use librepology::v1::types::Repo;
 use failure::Fallible as Result;
 use prettytable::format;
 use prettytable::Table;
 
 use crate::frontend::Frontend;
+use crate::backend::Backend;
+use librepology::v1::api::Api;
 
 pub struct TableFrontend(Stdout);
 
@@ -84,6 +87,42 @@ impl Frontend for TableFrontend {
                 problem.problem_description()
             ]);
         });
+
+        let mut outlock = self.0.lock();
+        table.print(&mut outlock)?;
+
+        Ok(())
+    }
+
+    fn compare_packages(&self, packages: Vec<Package>, backend: &Backend, filter_repos: Vec<Repo>) -> Result<()> {
+        let mut table = Table::new();
+        let format = format::FormatBuilder::new()
+            .column_separator('|')
+            .borders('|')
+            .separators(
+                &[format::LinePosition::Title, format::LinePosition::Top, format::LinePosition::Bottom],
+                format::LineSeparator::new('-', '+', '+', '+')
+            )
+            .padding(1, 1)
+            .build();
+        table.set_format(format);
+
+        table.set_titles(row!["Name", "Local Version", "Repo", "Upstream Version"]);
+
+        for package in packages {
+            backend
+                .project(package.name().deref())?
+                .into_iter()
+                .filter(|p| filter_repos.contains(p.repo()))
+                .for_each(|upstream_package| {
+                    table.add_row(row![
+                       package.name().deref().clone(),
+                         package.version().deref().clone(),
+                         upstream_package.repo().deref().clone(),
+                         upstream_package.version().deref().clone(),
+                   ]);
+                });
+        }
 
         let mut outlock = self.0.lock();
         table.print(&mut outlock)?;
