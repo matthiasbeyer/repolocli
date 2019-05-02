4d14473635b077755368c57497af9c20f9c30254
Matthias Beyer <matthias.beyer@atos.net>
2019-05-02 16:21:11 +0200

    Revert "Add filters for Repo, Name, Status, Version, License and Maintainer"
    We do not really need this feature in the library, authors of software
    can easily implement this for their needs and we do not have to ship all
    features all people _might_ use at some point.
    
    Keep the library KISSy.
    
    This reverts commit 44215217da63762b36827f5ef6604c5a2ad6b73d.

--- librepology/src/lib.rs
+++ librepology/src/lib.rs
@@ -5,16 +5,9 @@ extern crate url;
 extern crate url_serde;
 extern crate curl;
 
-#[cfg(feature = "packagefilters")]
-extern crate filters;
-#[cfg(feature = "packagefilters")]
-#[macro_use] extern crate derive_new;
-
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate log;
 #[macro_use] extern crate derive_more;
+#[macro_use] extern crate derive_new;
 
 pub mod v1;
-
-#[cfg(feature = "packagefilters")]
-pub mod packagefilters;
